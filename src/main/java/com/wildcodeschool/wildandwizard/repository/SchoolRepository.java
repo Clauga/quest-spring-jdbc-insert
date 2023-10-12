05a5a6cdf3d6cefdb571c0b37e2c2eeadd79eb7f
clauga <guerrero.ayala.cs@gmail.com>
2023-10-12 16:43:53 +0200

    quest insert done

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,7 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import util.JdbcUtils;
 
 import java.sql.*;
 
@@ -11,26 +12,39 @@ public class SchoolRepository {
     private final static String DB_PASSWORD = "Horcrux4life!";
 
     public School save(String name, Long capacity, String country) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            String insertQuery = "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)";
-            String[] generatedColumns = {"id"};
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet generatedKeys = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, name);
+            statement.setLong(2, capacity);
+            statement.setString(3, country);
 
-            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery, generatedColumns)) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setLong(2, capacity);
-                preparedStatement.setString(3, country);
-                preparedStatement.executeUpdate();
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to insert data");
+            }
 
-                try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
-                    if (resultSet.next()) {
-                        Long id = resultSet.getLong(1);
-                        return new School(id, name, capacity, country);
+            generatedKeys = statement.getGeneratedKeys();
 
-                    }
-                }
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getLong(1);
+                return new School(id, name, capacity, country);
+            } else {
+                throw new SQLException("failed to get inserted id");
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(generatedKeys);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
         }
         return null;
     }
